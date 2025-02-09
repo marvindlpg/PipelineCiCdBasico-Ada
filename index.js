fbfaf661c8d365814a2084f3910bc6d03c432903
unknown <marvin.dlpg@gmail.com>
2025-02-09 09:51:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+
+const apiRouter = require('./routes/api');
+
+const app =  express(); 
+
+require('./db');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', (req,res) =>{ 
+    res.send('Ada - Proceso de Selección Ing. DevOps'); 
+}); 
+
+
+app.use('/api', apiRouter);
+
+
+
+app.listen (3001, () => {
+   console.log('Servidor runners');
+});
+
